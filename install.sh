393097e67e6595d8cb335ee5072b832f0c37539e
Hanzo Huang <h1300923175@gmail.com>
2024-11-02 20:35:06 +0800

    add silent configuration

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 # Update package list
 echo "Updating package list..."
-sudo apt update
+sudo apt update -qq
 
 # Install required packages
 echo "Installing zsh, git, curl, and wget..."
-sudo apt install -y zsh git curl wget
+sudo apt install -qq -y zsh git curl wget
 
 # Change the default shell to zsh
 echo "Changing default shell to zsh..."
@@ -15,12 +15,12 @@ chsh -s $(which zsh)
 # Install antigen
 echo "Installing antigen"
 mkdir $HOME/.antigen
-curl -L git.io/antigen >$HOME/.antigen/antigen.zsh
+curl -s -L git.io/antigen >$HOME/.antigen/antigen.zsh
 
 # Import configuration
 echo "Importing configuration"
-curl -L raw.githubusercontent.com/Hanzo-Huang/Build-Perfect-Workspace/refs/heads/main/zshrc >$HOME/.zshrc
-curl -L raw.githubusercontent.com/Hanzo-Huang/Build-Perfect-Workspace/refs/heads/main/antigenrc >$HOME/.antigenrc
+curl -s -L raw.githubusercontent.com/Hanzo-Huang/Build-Perfect-Workspace/refs/heads/main/zshrc >$HOME/.zshrc
+curl -s -L raw.githubusercontent.com/Hanzo-Huang/Build-Perfect-Workspace/refs/heads/main/antigenrc >$HOME/.antigenrc
 
 # reload shell
 exec zsh
